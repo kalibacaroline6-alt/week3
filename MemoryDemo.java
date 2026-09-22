b9f1144c31ec4e0942e79bb16b04ac74fff3227f
kalibacaroline6-alt <kalibacaroline6@gmail.com>
2026-09-22 23:25:36 +0200

    Explore primitive vs reference behaviour

---
+++ MemoryDemo.java
@@ -0,0 +1,39 @@
+public class MemoryDemo {
+
+    static class BankAccount {
+        int balance;
+    }
+
+    public static void main(String[] args) {
+
+        BankAccount acc1 = new BankAccount();
+        acc1.balance = 1000;
+
+        BankAccount acc2 = acc1;
+        acc2.balance = 500;
+
+        System.out.println("acc1: " + acc1.balance);
+        System.out.println("acc2: " + acc2.balance);
+
+        BankAccount myAccount = new BankAccount();
+        myAccount.balance = 500;
+
+        applyBonusToAccount(myAccount);
+
+        System.out.println("myAccount: " + myAccount.balance);
+
+        int cash = 500;
+
+        applyBonusToAmount(cash);
+
+        System.out.println("cash: " + cash);
+    }
+
+    public static void applyBonusToAccount(BankAccount acc) {
+        acc.balance = acc.balance + 100;
+    }
+
+    public static void applyBonusToAmount(int amount) {
+        amount = amount + 100;
+    }
+}
