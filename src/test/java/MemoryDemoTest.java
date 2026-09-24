a595d08ef4f4f08f13832113206540331d527689
kalibacaroline6-alt <kalibacaroline6@gmail.com>
2026-09-24 22:08:35 +0200

    Add Maven tests for object sharing

---
+++ src/test/java/MemoryDemoTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MemoryDemoTest {
+
+    @Test
+    void separateObjectsDoNotShareState() {
+        BankAccount acc1 = new BankAccount();
+        BankAccount acc2 = new BankAccount();
+
+        acc1.balance = 500;
+
+        assertEquals(0, acc2.balance);
+    }
+
+    @Test
+void aliasedObjectsShareState() {
+    BankAccount acc1 = new BankAccount();
+
+    acc1.balance = 500;
+
+    BankAccount acc2 = acc1;
+    acc2.balance = 750;
+
+    assertEquals(750, acc1.balance);
+}
+}
