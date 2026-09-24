fa40887d25228883e96a7de7497c8237d9070df8
kalibacaroline6-alt <kalibacaroline6@gmail.com>
2026-09-24 19:10:22 +0200

    Handle null with a professional check

--- MemoryDemo.java
+++ MemoryDemo.java
@@ -27,6 +27,14 @@ public class MemoryDemo {
         applyBonusToAmount(cash);
 
         System.out.println("cash: " + cash);
+
+        BankAccount acc3 = null;
+        
+        if (acc3 != null) {
+            System.out.println(acc3.balance);
+        } else {
+            System.out.println("Account not found");
+        }
     }
 
     public static void applyBonusToAccount(BankAccount acc) {
